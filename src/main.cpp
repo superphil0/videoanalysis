9dffc88890bcd21f0eed89448a47018fedc9464f
Philipp Omenitsch <philippomenitsch@gmx.at>
2015-04-26 20:58:29 +0200

    added argument parsing and file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,44 @@
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
-int main( int argc, char** argv )
+#include "main.h"
+int main(int argc, char** argv)
 {
- const char* imageName = std::string("/Users/markomlinaric/Desktop/test.png").c_str();
+	if (argc < 5)
+	{
+		cout << "Not enough arguments" << endl;
+		return 1;
+	}
+	string path, filename;
+	int frames, initframes;
+	path = argv[1];
+	filename = argv[2];
+	frames = atoi(argv[3]);
+	initframes = atoi(argv[3]);
+	processVideo(path, filename, frames, initframes);
+	return 0;
+}
 
- cv::Mat image;
- image = cv::imread( imageName, 1 );
-
- if(!image.data )
- {
-   printf( " No image data \n " );
-   return -1;
- }
-
- cv::Mat gray_image;
- cv::cvtColor( image, gray_image, cv::COLOR_BGR2GRAY );
-
- //cv::imwrite( "../../images/Gray_Image.jpg", gray_image );
-
- cv::namedWindow( imageName, cv::WINDOW_AUTOSIZE );
- cv::namedWindow( "Gray image", cv::WINDOW_AUTOSIZE );
-
- cv::imshow( imageName, image );
- cv::imshow( "Gray image", gray_image );
-
- cv::waitKey(0);
-
- return 0;
+int processVideo(string path, string filename, int frames, int initframes)
+{
+	cv::Mat image;
+	// read files in loop
+	for (int i = 0; i < frames; i++)
+	{
+		ostringstream ss;
+		ss << setw(4) << setfill('0') << i;
+		string str_i(ss.str());
+		string fullpath = path + PATH_SEPARATOR + filename + "_" + str_i;
+		image = cv::imread(fullpath, 1);
+		processFrame(image);
+		string s = "Processing frame " + str_i+"\n";
+		printf(s.c_str());
+	}
+}
+void processFrame(cv::Mat image)
+{
+	cv::Mat gray_image;
+	// convert to grayscale
+	//cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
+	//cv::imwrite( "../../images/Gray_Image.jpg", gray_image );
+	//cv::namedWindow(imageName, cv::WINDOW_AUTOSIZE);
+	//cv::namedWindow("Gray image", cv::WINDOW_AUTOSIZE);
+	//cv::imshow(imageName, image);
 }
