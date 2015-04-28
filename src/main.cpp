cb63378cf03994cdccb0fa5d3861ab1e7fe0c1e8
Philipp Omenitsch <philippomenitsch@gmx.at>
2015-04-28 23:45:41 +0200

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,6 @@ int processVideo(string path, string filename, int frames, int initframes)
 	cv::Mat image;
 	string fullpath = path + PATH_SEPARATOR + filename + "_0000.jpeg";
 	image = cv::imread(fullpath, CV_LOAD_IMAGE_COLOR);
-	//image.convertTo(image, CV_32FC3);
 	ref_mean = cv::Mat::zeros(image.rows, image.cols,CV_32FC3);
 	ref_var = cv::Mat::zeros(image.rows, image.cols,CV_32FC3);
 	// read files in loop
@@ -34,7 +33,7 @@ int processVideo(string path, string filename, int frames, int initframes)
 		ss << setw(4) << setfill('0') << i;
 		string str_i(ss.str());
 		string fullpath = path + PATH_SEPARATOR + filename + "_" + str_i +".jpeg";
-		image = cv::imread(fullpath, CV_LOAD_IMAGE_COLOR);
+		image = cv::imread(fullpath, cv::IMREAD_COLOR);
 		if (!image.data)
 		{
 			printf(" No image data \n ");
@@ -61,12 +60,12 @@ cv::Mat processFrame(cv::Mat image, int learnframes, int framenum)
 	// online calculation http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm
 		float frame = framenum + 1.0f;
 		subtract(image, ref_mean, delta);
-		add(ref_mean, delta*(1/frame), ref_mean);
+		add(ref_mean, delta.mul(1.0f/frame), ref_mean);
 		subtract(image, ref_mean, tmp);
 		add(ref_var, delta.mul(tmp), ref_var);
 		if (framenum == learnframes - 1)
 		{
-			ref_var.mul(learnframes - 1);
+			ref_var = ref_var.mul(1.0f/(learnframes - 1));
 			sqrt(ref_var, tmp);
 			ref_var = tmp;
 		}
@@ -78,30 +77,33 @@ cv::Mat processFrame(cv::Mat image, int learnframes, int framenum)
 		cv::absdiff(image, ref_mean, delta);
 		// constant = how many sigmas distance
 		tmp = ref_var*3.5f;
-		tmp.convertTo(tmp, image.type());
 		tmp = delta >= tmp;
 		cv::cvtColor(tmp, tmp, cv::COLOR_BGR2GRAY);
-		cv::namedWindow("haha", cv::WINDOW_AUTOSIZE);
-		cv::imshow("haha", tmp);
-		//cv::GaussianBlur(tmp, tmp, cv::Size(3, 3), 0, 0);
+		cv::GaussianBlur(tmp, tmp, cv::Size(3, 3), 0, 0);
 		cv::threshold(tmp, tmp, 200.0, 255, cv::THRESH_BINARY);
-		cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5));
-		//cv::morphologyEx(tmp, tmp, cv::MORPH_OPEN, element);
+		cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+		cv::morphologyEx(tmp, tmp, cv::MORPH_OPEN, element);
+
 		// adapt model
 		// calculate coefficient alpha per pixel
 		// in form of: alpha for 0 (background) and 1-alpha for 1 (foreground)
-		float alpha = 0.1f;
+		float fgAlpha = 0.1f;
+		float bgAlpha = 1.0f-fgAlpha;
 		tmp.copyTo(delta);
 		delta.convertTo(delta, CV_32F);
-		delta = delta + (1-alpha);
-		cv::threshold(delta, delta,max(alpha,1-alpha),  1 - alpha, cv::THRESH_TRUNC);
+		cv::Mat mask = delta == 0;
+		delta.setTo(bgAlpha, mask);
+		mask = delta >= 0.99999f;
+		delta.setTo(fgAlpha, mask);
 
 		cv::cvtColor(delta,delta, CV_GRAY2BGR,3);
 		cv::Mat reciAlpha, change;
 		cv::subtract(cv::Scalar::all(1.0f), delta, reciAlpha);
+		cv::namedWindow("haha", cv::WINDOW_AUTOSIZE);
+		cv::imshow("haha", reciAlpha);
 		cv::absdiff(image, ref_mean, change);
-		ref_var = ref_var.mul(delta) + delta.mul(change);
-		ref_mean = ref_mean.mul(delta) + image.mul(reciAlpha);
+		ref_var = ref_var.mul(reciAlpha) + delta.mul(change);
+		ref_mean = ref_mean.mul(reciAlpha) + image.mul(delta);
 		if (DEBUG)
 		{
 			cv::Mat tmpMean, tmpVar, tmpTmp, tmpImage;
@@ -117,7 +119,7 @@ cv::Mat processFrame(cv::Mat image, int learnframes, int framenum)
 			cv::imshow("Var", tmpVar);
 			cv::imshow("Seg", tmpTmp);
 			cv::imshow("Org", tmpImage);
-			cv::waitKey();
+			cv::waitKey(10);
 		}
 		tmp.convertTo(tmp, CV_8UC3);
 		return tmp;
